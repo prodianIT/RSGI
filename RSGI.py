8846b382d3be7663901e8e984bcd12d3a7c0c5c3
prodianIT <119439404+prodianIT@users.noreply.github.com>
2022-12-04 20:06:18 +0530

    Update RSGI.py

--- RSGI.py
+++ RSGI.py
@@ -3,13 +3,14 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import altair as alt
 import numpy as np
+from datetime import date
 
 st.set_page_config(page_title="RSGI Dashboard", page_icon=":bar_chart:", layout="wide")
 # ---- SIDEBAR ----
 # with st.sidebar.header('1. Upload your CSV data'):
 #     uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['csv'])
 csv_files = ['data.000.csv', 'data.001.csv', 'data.002.csv', 'data.003.csv', 'data.004.csv', 'data.005.csv', 'data.006.csv', 'data.007.csv', 'data.008.csv', 'data.009.csv']
-
+this_year = todays_date.year
 
 li = []
 
@@ -63,7 +64,7 @@ a = df.query(
 # ---- MAINPAGE ----
 st.title(":bar_chart: RSGI Dashboard")
 st.markdown("##")
-Age_of_vehicle = st.select_slider(
+year_of_manufacture = st.select_slider(
     'Select the year of manufacture of the vehicle',
     options=a["year_of_manufacture"].unique(),
     value=(2018))
@@ -71,25 +72,27 @@ st.write('You selected the year', Age_of_vehicle )
 option_list = ['amount paid', 'claim count', 'repair amount', 'parts net amount']
 result = st.selectbox('select your analysis category', option_list)
 
-# if result == 'amount paid':
-#     # claim amount
-#     st.header('*amount_paid*')
-#     st.caption('amount in crores')
-#     b = a.pivot_table(values='amount_paid_y',
-#                       index=['year_month', 'Ageofthevehicle'],
-#                       columns=['reapairer_type'],
-#                       aggfunc='sum')
-#     b.reset_index(inplace=True)
-#     b.columns = ['year_month', 'Ageofthevehicle', 'dealer', 'OTHERS', 'TRS']
-#     b['dealer'] = b['dealer'] / 10000000
-#     b['OTHERS'] = b['OTHERS'] / 10000000
-#     b['TRS'] = b['TRS'] / 10000000
-#     b['dealer_%'] = (b['dealer'] / b['dealer'].sum()) * 100
-#     b['OTHERS_%'] = (b['OTHERS'] / b['OTHERS'].sum()) * 100
-#     b['TRS_%'] = (b['TRS'] / b['TRS'].sum()) * 100
-#     b = b[['year_month', 'Ageofthevehicle', 'dealer', 'dealer_%', 'OTHERS', 'OTHERS_%', 'TRS',
-#            'TRS_%']]
-#     b
+if result == 'amount paid':
+    # claim amount
+    st.header('*amount_paid*')
+    st.caption('amount in crores')
+    b = a.pivot_table(values='amount_paid_y',
+                      index=['year_month', 'Ageofthevehicle'],
+                      columns=['reapairer_type'],
+                      aggfunc='sum')
+    b.reset_index(inplace=True)
+    b.columns = ['year_month', 'Ageofthevehicle', 'dealer', 'OTHERS', 'TRS']
+    b['dealer'] = b['dealer'] / 10000000
+    b['OTHERS'] = b['OTHERS'] / 10000000
+    b['TRS'] = b['TRS'] / 10000000
+    b['dealer_%'] = (b['dealer'] / b['dealer'].sum()) * 100
+    b['OTHERS_%'] = (b['OTHERS'] / b['OTHERS'].sum()) * 100
+    b['TRS_%'] = (b['TRS'] / b['TRS'].sum()) * 100
+    b = b[['year_month', 'Ageofthevehicle', 'dealer', 'dealer_%', 'OTHERS', 'OTHERS_%', 'TRS',
+           'TRS_%']]
+    age = this_year -  year_of_manufacture 
+    st.write(' age of the vehicle >', age)
+    b
 
 
 # if result == 'claim count':
